feb3d7e5126476715fd8c70f4dabb838476228ab
Cristian Sammarco <cristiansammarco2005@gmail.com>
2025-10-30 20:07:14 +0100

    Aggiornamento X o nuova funzionalità

--- FinView/ContentView.swift
+++ FinView/ContentView.swift
@@ -5,20 +5,72 @@
 //  Created by Cristian Sammarco on 30/10/25.
 //
 
-import SwiftUI
+import SwiftUI // 1. Importa il framework di Apple
+import Charts
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct ContentView: View { // 2. La tua schermata
+    var body: some View { // 3. La proprietà body (il contenuto visuale)
+        ZStack {
+            Color.white.ignoresSafeArea() //colore background
+
+            VStack(alignment: .leading) { //VStack per inserire i testi
+                
+                //Testo Saldo totale
+                Text("Saldo Totale")
+                    .bold()
+                    .italic()
+                    .font(.title)
+                    .foregroundColor(.black)
+                    .padding()
+                
+                HStack {
+                    
+                    //Testo Saldo calcolato
+                    Text("2010")
+                        .font(.title2)
+                        .bold()
+                        .foregroundColor(.black)
+                        .padding(.leading)
+                        //.frame(width: 1, height: 1, alignment: .center)
+                    
+                    //Testo €
+                    Text("€")
+                        .bold()
+                        .foregroundColor(.black)
+                        .font(.title2)
+                        //.frame(width: 1, height: 1, alignment: .center)
+                        
+                }
+                
+                /*HStack{ //Uso HStack per centrare la Card
+                    
+                    Spacer() //Spacer per centrare
+                    
+                    //Card
+                    VStack {
+                        Text("Prova")
+                            .foregroundColor(.black)
+                    }
+                    .frame(width: 360, height: 250, alignment: .center)
+                    .padding(.leading)
+                    .background(RoundedRectangle(cornerRadius: 20, style: .circular)
+                        .fill(Color.white)
+                        .shadow(color: Color.black.opacity(0.3), radius: 12, x: 0, y: 6))
+                    
+                    Spacer() //Spacer per centrare
+                }
+                .padding()*/
+                
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+            .padding()
+            
         }
-        .padding()
+        
     }
 }
 
-#Preview {
+
+#Preview { // 4. La funzione di Anteprima (Preview)
     ContentView()
 }
